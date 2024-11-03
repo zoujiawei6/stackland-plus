9ca8f5e17610fdab8ffe9498fbf2bf2a9fefe155
Zoujiawei <1037902662@qq.com>
2024-11-03 23:56:53 +0800

    超级农场-10%

---
+++ Scripts/Workshop/SuperFarmWorkshop.cs
@@ -0,0 +1,10 @@
+namespace ZjaveStacklandsPlus.Scripts
+{
+  public class SuperFarmWorkshop : Greenhouse
+  {
+    public static string cardId = "zjave_super_farm_workshop";
+    public static string blueprintId = "zjave_blueprint_super_farm_workshop";
+
+  }
+
+}
