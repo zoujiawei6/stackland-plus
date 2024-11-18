16f1d63e4390b4356dce628cb82c5497ae82a1fc
Zoujiawei <1037902662@qq.com>
2024-11-18 23:19:59 +0800

    放弃自动化的想法吧，进阶卡牌才是硬道理

---
+++ Scripts/Workshops/IconBarWorkshop.cs
@@ -0,0 +1,10 @@
+namespace ZjaveStacklandsPlus.Scripts.Workshops
+{
+  /// <summary>
+  /// 铁矿石 = 1x铁矿, 1村民45s
+  /// 铁锭 = 1x铁矿石, 1x木材在冶炼炉中10 秒
+  /// </summary>
+  public class IconBarWorkshop : CardData
+  {}
+
+}
